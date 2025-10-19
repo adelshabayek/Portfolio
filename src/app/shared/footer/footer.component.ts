9baf6bbeaa3c27efbaa3792d33b30d033a70edd7
adel shabayek <pildexyt@gmail.com>
2025-10-19 15:27:03 +0300

    sad

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { ProfileService } from '../../core/services/profile.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  contact: any;
 
+  constructor(private profileService: ProfileService) {}
+  currentYear: number = new Date().getFullYear();
+
+  ngOnInit(): void {
+    this.profileService.getProfile().subscribe((data) => {
+      this.contact = data.contact;
+    });
+  }
 }
